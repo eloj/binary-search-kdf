71b67ef795791163b5dd5d9462c645a96b1e9a4b
Eddy Jansson <eddy@klopper.net>
2020-08-16 19:38:37 +0200

    A bit of scaffolding, plus linear search.
    I'd prefer to not have to make a class, or a state struct,
    but we'll see.

--- lutbs-test.cpp
+++ lutbs-test.cpp
@@ -22,15 +22,13 @@
 #include <array>
 
 template<int LUTBits = 16, typename T, typename KeyFunc = decltype(kdf<T>)>
-const std::vector<size_t> build_bs_lut(T *arr, size_t len, uint8_t extra_shift, KeyFunc && kf = kdf) {
+const std::vector<size_t> build_bs_lut(const T *arr, size_t len, uint8_t extra_shift = 0, KeyFunc && kf = kdf) {
 	constexpr size_t lutlen = 1UL << LUTBits;
+	const uint32_t shift = ((sizeof(T) << 3) - LUTBits) - extra_shift;
+
 	std::vector<size_t> lut(lutlen + 1); // Add one as sentinel for now.
 
-	uint32_t shift = ((sizeof(T) << 3) - LUTBits);
-
 	printf("Building LUT<%d> with %zu entries, shift=%d, extra_shift=%d\n", LUTBits, lutlen, shift, extra_shift);
-
-	shift -= extra_shift;
 
 	size_t bucket = 0;
 	size_t lo = 0;
@@ -56,6 +54,35 @@ const std::vector<size_t> build_bs_lut(T *arr, size_t len, uint8_t extra_shift, 
 	}
 
 	return lut;
+}
+
+// We'd should maybe consider packaging the lut in an opaque type with LUTBits and extra_shift, maybe a ref to the KeyFunc?
+template<typename T, typename KeyFunc = decltype(kdf<T>)>
+size_t lower_bound_lut(const T* arr, size_t len, const std::vector<size_t>& lut, T key, uint8_t extra_shift = 0, KeyFunc && kf = kdf) {
+	assert(lut.size() > 0);
+	assert(lut.size() & 1);
+
+	const int LUTBits = __builtin_ctz(lut.size() & (lut.size()-1)); // Mask out lowest bit due to sentinel.
+	const uint32_t shift = ((sizeof(T) << 3) - LUTBits) - extra_shift;
+
+	printf("lut size=%zu, derived lut bits: %d, lut shift=%d\n", lut.size(), LUTBits, shift);
+
+	auto keyval = kf(key);
+	size_t bucket = keyval >> shift;
+	size_t l = lut[bucket];
+	size_t r = lut[bucket+1];
+
+	assert(l <= r);
+
+	printf("key=%x, kf(key)=%x, bucket=%zu, searching range [%zu, %zu)\n", key, keyval, bucket, l, r);
+
+	// Linear scan for small ranges.
+	for (size_t i = l ; i < r ; ++i) {
+		if (arr[i] == key)
+			return i;
+	}
+
+	return len; // I think this is what std::l_b does...
 }
 
 template<typename T, typename KeyFunc = decltype(kdf<T>)>
@@ -86,7 +113,8 @@ const std::vector<T> random_array(size_t len, GenFunc && gen) {
 }
 
 int main(int argc, char *argv[]) {
-	static const int N = 20;
+	constexpr int N = 20;
+	constexpr int LBITS = 5;
 
 	// std::random_device random_device();
 	std::default_random_engine gen; // random_device()
@@ -103,11 +131,11 @@ int main(int argc, char *argv[]) {
 
 	for (size_t i = 0 ; i < input.size() ; ++i) {
 		auto val = input[i];
-		printf("[%04zu] %08u (%08x -> kdf -> bucket %04x)\n", i, val, val, kdf(val) >> (32 - 5 - extra_shift));
+		printf("[%04zu] %08u (%08x -> kdf -> bucket %04x)\n", i, val, val, kdf(val) >> (sizeof(val)*8 - LBITS - extra_shift));
 	}
 
-	auto lut = build_bs_lut<5>(input.data(), input.size(), extra_shift);
-	// auto lut = build_bs_lut<5>(input.data(), input.size(), extra_shift, [](const uint32_t& value){ return value + 1; } );
+	auto lut = build_bs_lut<LBITS>(input.data(), input.size(), extra_shift);
+	// auto lut = build_bs_lut<LBITS>(input.data(), input.size(), extra_shift, [](const uint32_t& value){ return value + 1; } );
 
 	for (size_t i = 0 ; i < lut.size() - 1 ; ++i) {
 		size_t keys = 0;
@@ -116,14 +144,18 @@ int main(int argc, char *argv[]) {
 		printf("LUT[%.*lx] = %04zu keys in [%zu..%zu)\n", 4, i, keys, lut[i], lut[i+1]);
 	}
 
-	uint32_t key = input[N-7];
+	uint32_t key = argc > 1 ? uint32_t(atoi(argv[1])) : input[N-7];
 
 	auto low = std::lower_bound(begin(input), end(input), key);
 
 	printf("std::lower_bound for key %u is at position %zu\n", key, low - begin(input));
 
-	// TODO ...
-	// auto idx = lower_bound_lut(input.data(), input.size(), lut, key);
-	// printf("our lower_bound for key %u is at position %zu\n", key, idx);
+	auto idx = lower_bound_lut(input.data(), input.size(), lut, key, extra_shift);
+	printf("our lower_bound for key %u is at position %zu\n", key, idx);
+	if (idx > input.size() - 1) {
+		printf("KEY NOT FOUND.\n");
+	} else if (idx != size_t(low - begin(input))) {
+		printf("KEY MISMATCH ERROR!\n");
+	}
 
 }
