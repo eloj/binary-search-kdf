59870f1397a3da2e30c0ab60dd43764166399394
Eddy Jansson <eddy@klopper.net>
2020-08-17 23:11:28 +0200

    Make LUT size a standard argument.
    Change lookup return value to match std::lower_bound()
    
    More debug logging.

--- lutbs-test.cpp
+++ lutbs-test.cpp
@@ -22,14 +22,14 @@
 #include <vector>
 #include <array>
 
-template<int LUTBits = 16, typename T, typename KeyFunc = decltype(kdf<T>)>
-const std::vector<size_t> build_bs_lut(const T *arr, size_t len, uint8_t extra_shift = 0, KeyFunc && kf = kdf) {
-	constexpr size_t lutlen = 1UL << LUTBits;
-	const uint32_t shift = ((sizeof(T) << 3) - LUTBits) - extra_shift;
+template<typename T, typename KeyFunc = decltype(kdf<T>)>
+const std::vector<size_t> build_bs_lut(int lutbits, const T *arr, size_t len, uint8_t extra_shift = 0, KeyFunc && kf = kdf) {
+	const size_t lutlen = 1UL << lutbits;
+	const uint32_t shift = ((sizeof(T) << 3) - lutbits) - extra_shift;
 
 	std::vector<size_t> lut(lutlen + 1); // Add one as sentinel for now.
 
-	printf("Building LUT<%d> with %zu entries, shift=%d, extra_shift=%d from %zu entries.\n", LUTBits, lutlen, shift, extra_shift, len);
+	printf("Building %zu byte LUT<%d> with %zu entries. shift=%d, extra_shift=%d\n", sizeof(lut[0])*lut.size(), lutbits, lutlen, shift, extra_shift);
 
 	size_t bucket = 0;
 	size_t lo = 0;
@@ -94,7 +94,7 @@ static size_t lower_bound_internal(const T* arr, size_t len, T key, size_t l, si
 
 	printf("number of probes: %zu\n", n_probes);
 
-	return (len && arr[l] == key) ? l : len; // NOTE: Returns OOB if key not found, similar to std::lower_bound
+	return l; // Returns position where key would go. Caller must verify.
 }
 
 
@@ -107,12 +107,12 @@ size_t lower_bound_lut(const T* arr, size_t len, const std::vector<size_t>& lut,
 	const int LUTBits = __builtin_ctz(lut.size() & (lut.size()-1)); // Mask out lowest bit due to sentinel.
 	const uint32_t shift = ((sizeof(T) << 3) - LUTBits) - extra_shift;
 
-	printf("lut size=%zu, derived lut bits: %d, lut shift=%d\n", lut.size(), LUTBits, shift);
+	// printf("lut size=%zu, derived lut bits: %d, lut shift=%d\n", lut.size(), LUTBits, shift);
 
 	auto keyval = kf(key);
 	size_t bucket = keyval >> shift;
 	size_t l = lut[bucket];
-	size_t r = lut[bucket+1] - 1;
+	size_t r = lut[bucket+1];
 
 	assert(l <= r);
 
@@ -157,7 +157,7 @@ int main(int argc, char *argv[]) {
 	typedef uint32_t ArrT;
 
 	const int N = argc > 1 ? uint32_t(atoi(argv[1])) : 200;
-	constexpr int LBITS = 5;
+	const int LBITS = argc > 2 ? atoi(argv[2]) : 5;
 
 	// std::random_device random_device();
 	std::default_random_engine gen; // random_device()
@@ -179,34 +179,37 @@ int main(int argc, char *argv[]) {
 		printf("[%04zu] %08u (%08x -> kdf -> bucket %04x)\n", i, val, val, kdf(val) >> (sizeof(val)*8 - LBITS - extra_shift));
 	}
 
-	auto lut = build_bs_lut<LBITS>(input.data(), input.size(), extra_shift);
-	// auto lut = build_bs_lut<LBITS>(input.data(), input.size(), extra_shift, [](const uint32_t& value){ return value + 1; } );
+	auto lut = build_bs_lut(LBITS, input.data(), input.size(), extra_shift);
+	// auto lut = build_bs_lut(LBITS, input.data(), input.size(), extra_shift, [](const uint32_t& value){ return value; } );
 
+	size_t utilization = 0;
 	for (size_t i = 0 ; i < lut.size() - 1 ; ++i) {
 		size_t keys = 0;
 		if (lut[i] != lut[i+1])
 			keys = lut[i+1] - lut[i];
-		if (keys > 0)
+		if (keys > 0) {
 			printf("LUT[%.*lx] = %04zu keys in [%zu..%zu)\n", 4, i, keys, lut[i], lut[i+1]);
+			++utilization;
+		}
+	}
+	printf("%zu of %zu LUT entries occupied.\n", utilization, lut.size() - 1);
+
+	ArrT key = argc > 3 ? atoi(argv[3]) : input[N-7];
+
+	size_t low = std::lower_bound(begin(input), end(input), key) - begin(input);
+	printf("LOOKUP 1: std::lower_bound() for key %u is at position %zu: %s\n", key, low, low < input.size() && input[low] == key ? "found" : "NOT found");
+
+	size_t idx = lower_bound_lut(input.data(), input.size(), lut, key, extra_shift);
+	printf("LOOKUP 2: lower_bound_lut()  for key %u is at position %zu: %s\n", key, idx, idx < input.size() && input[idx] == key ? "found" : "NOT found");
+
+	if (idx == low) {
+		printf("Lookups match.\n");
+	} else {
+		printf("KEY INDEX MISMATCH ERROR! -- std::lower_bound and our search does not agree!\n");
+		abort();
 	}
 
-	printf("LUT is %zu bytes + sentinel.\n", sizeof(size_t)*(lut.size()-1));
-
-	ArrT key = argc > 2 ? atoi(argv[2]) : input[N-7];
-
-	auto low = std::lower_bound(begin(input), end(input), key);
-
-	printf("std::lower_bound for key %u is at position %zu\n", key, low - begin(input));
-
-	auto idx = lower_bound_lut(input.data(), input.size(), lut, key, extra_shift);
-	printf("our lower_bound for key %u is at position %zu\n", key, idx);
-	if (idx > input.size() - 1) {
-		printf("KEY NOT FOUND.\n");
-	} else if (idx != size_t(low - begin(input))) {
-		printf("KEY MISMATCH ERROR!\n");
-	}
-
-	printf("Standard binary search ");
+	printf("; Standard binary search ");
 	lower_bound_internal(input.data(), input.size(), key, 0, input.size() - 1);
 
 }
